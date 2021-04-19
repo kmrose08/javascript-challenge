d7dee8e2fc5e146f80200c461556fe96e3778f3c
Kelly <kmrose08@gmail.com>
2021-04-19 18:14:22 -0400

    updated files

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# javascript-challenge
+JavaScript and DOM Manipulation
+
+Automatic Table and Date Search 
+
+
+* Using the UFO dataset provided in the form of an array of JavaScript objects, wrote code that appends a table to the web page and then adds new rows of data for each UFO sighting.
+
+* Wrote JavaScript code that will listen for events and search through the `date/time` column to find rows that match user input.
+
+
--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,54 @@
 // from data.js
-var tableData = data;
+var details = data;
 
-// YOUR CODE HERE!
+// Create empty arrays to store the values
+var datetime = [];
+var city = [];
+var state = [];
+var country = [];
+var shape = [];
+var durationMinutes = [];
+var comments = [];
+
+// Use map to build arrays
+var datetimeMapped = details.map(detail => detail.datetime);
+var cityMapped = details.map(detail => detail.city);
+var stateMapped = details.map(detail => detail.state);
+var countryMapped = details.map(detail => detail.country);
+var shapeMapped = details.map(detail => detail.shape);
+var durationMapped = details.map(detail => detail.duration);
+
+var tbody = d3.select("tbody");
+
+function buildTable(data) {
+  tbody.html('');
+  data.forEach(function (sighting) {
+  var row = tbody.append("tr");
+  Object.entries(sighting).forEach(function ([key, value]) {
+    var cell = row.append("td");
+    cell.text(value);
+  });
+})};
+
+
+function handleClick() {
+
+  
+  let date = d3.select("#datetime").property("value");
+  let filterData = data;
+  
+  
+  if(date) {
+    
+    filterData = filterData.filter((row) => row.datetime === date);
+  }
+  
+  console.log(filterData);
+
+    buildTable(filterData);
+  }
+
+  d3.selectAll("#filter-btn").on("click", handleClick);
+  buildTable(details);
+
+  
